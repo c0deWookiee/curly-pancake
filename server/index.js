fcaf714fcb05f0780c52d98e1b816df0244885dd
Daniel Pham <eetandg@mail.ccsf.edu>
2019-08-13 15:02:57 -0700

    changed to single quotes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const port = process.env.PORT || 27015;
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
 
 //routes
-const usersRoute = require("./api/users.js");
-const profileRoute = require("./api/profile.js");
-const postRoute = require("./api/post.js");
-const authRoute = require("./api/auth.js");
+const usersRoute = require('./api/users.js');
+const profileRoute = require('./api/profile.js');
+const postRoute = require('./api/post.js');
+const authRoute = require('./api/auth.js');
 
 //database connection
-const connectDB = require("../config/db");
+const connectDB = require('../config/db');
 connectDB();
 
 const app = express();
@@ -18,11 +18,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //end points
-app.use("/api/users", usersRoute);
-app.use("/api/profile", profileRoute);
-app.use("/api/post", postRoute);
-app.use("/api/auth", authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/post', postRoute);
+app.use('/api/auth', authRoute);
 //serve static
-app.use(express.static(path.resolve(__dirname, "/../dist")));
+app.use(express.static(path.resolve(__dirname, '/../dist')));
 
 app.listen(port, () => console.log(`server running port: ${port}`));
