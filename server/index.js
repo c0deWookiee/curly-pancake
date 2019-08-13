c215f576e16ba0fff39cf5147e4066d161747856
Alexander Lee <4lexslee@gmail.com>
2019-08-13 13:25:47 -0700

    Database setup complete

--- server/index.js
+++ server/index.js
@@ -1,1 +1,20 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 27015;
+const bodyParser = require('body-parser');
+const app = express();
+const router = require('./routes.js');
+const connectDB = require('../config/db');
+
+connectDB();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', router);
+
+app.use(express.static(path.resolve(__dirname, '/../dist')));
+
+app.listen(port, () => {
+  console.log('hello on port ' + port);
+});
